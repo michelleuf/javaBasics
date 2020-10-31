676a3113e9bdc7b3955345f7cfe1f0ff35184e4d
SandaliPerera <56555623+SandaliPerera@users.noreply.github.com>
2020-10-31 23:39:46 +0530

    Create SringReverse.java

---
+++ SringReverse.java
@@ -0,0 +1,28 @@
+import java.util.Scanner; 
+import java.lang.*; 
+import java.io.*; 
+import java.util.*;
+   
+class StringReverse
+{ 
+    public static void main(String[] args) 
+    {
+        Scanner myObj = new Scanner(System.in);
+
+    System.out.println("Enter the word to reverse:");
+    String input = myObj.nextLine();
+
+        char[] x = input.toCharArray(); 
+        List<Character> y; 
+        y = new ArrayList<>();
+  
+        for (char c: x) 
+            y.add(c); 
+  
+        Collections.reverse(y); 
+        ListIterator a = y.listIterator(); 
+        
+        while (a.hasNext()) 
+            System.out.print(a.next()); 
+    } 
+}
